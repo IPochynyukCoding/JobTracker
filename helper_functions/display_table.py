60d887db992d72fe83ae2e61bdc4bfdcc9d903d7
IPochynyukCoding <157660033+IPochynyukCoding@users.noreply.github.com>
2026-09-03 21:54:34 -0500

    Removed URL from table

--- helper_functions/display_table.py
+++ helper_functions/display_table.py
@@ -6,8 +6,8 @@ def fetch_items(sql_cursor:sqlite3.Cursor,query:str):
     return sql_cursor.execute(query).fetchall()
 
 def create_table(sql_cursor:sqlite3.Cursor,sql_connector:sqlite3.Connection):
-    headers=["Job ID","Job Title","Last Updated","Job Status","Employer","Job Site","URL","Interview Date"]
-    search_query='''select job_id,job_title,last_updated_date, job_status.status_name, employer.employer_name, job_site.site_name,job_listing_url,job.interview_date
+    headers=["Job ID","Job Title","Last Updated","Job Status","Employer","Job Site","Interview Date"]
+    search_query='''select job_id,job_title,last_updated_date, job_status.status_name, employer.employer_name, job_site.site_name,job.interview_date
     from job left join employer on job.employer_id=employer.employer_id left join job_site on job.site_id=job_site.site_id left join job_status on job.job_status=job_status.job_status;'''
     sql_results:list[tuple]=fetch_items(sql_cursor,search_query)
     fetch_statuses=fetch_items(sql_cursor,"select status_name,hex_color from job_status")
